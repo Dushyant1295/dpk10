16f038bcbf8f969335e05a44473946fdf22c7436
Aayush Chaudhary <aayushcyy@gmail.com>
2025-10-01 19:22:38 +0530

    feat: creat structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import "../css/style.scss";
 import "swiper/css/bundle";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 import { isMobile } from "./Module/Helper";
 
 import { lenis, lenisScroll } from "./Module/lenis/lenis";
